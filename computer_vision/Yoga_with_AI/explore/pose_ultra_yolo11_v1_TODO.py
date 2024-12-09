451b3daff90e8618643f3c6d0dba10d540e09d59
ash322ash422 <160450942+ash322ash422@users.noreply.github.com>
2024-12-09 15:53:54 +0530

    Yoga pose detect and correct

--- computer_vision/Yoga_with_AI/explore/pose_ultra_yolo11_v1_TODO.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11n-pose.pt")
-
-# Train the model
-results = model.train(data="temp/senam_yoga_dataset11/data.yaml", epochs=2, imgsz=640)
-
